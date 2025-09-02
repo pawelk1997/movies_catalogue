b62b123c86ebe96eb7784f7fc1f8bda45bb848aa
Paweł Kępczyński <pawel.k1997@gmail.com>
2025-09-02 19:09:47 +0200

    Rozbudowanie projektu, wykonanie zadań

---
+++ tmdb_client.py
@@ -0,0 +1,53 @@
+import requests
+
+def get_popular_movies():
+    endpoint = "https://api.themoviedb.org/3/movie/popular"
+    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYmIyOTgxNjYyNTNhNjI3OGRkN2IxN2UxNDk1N2MwZiIsIm5iZiI6MTc1NjgwODIyMi45MzIsInN1YiI6IjY4YjZjNDFlYzBmYjRiNjA5YmI0ZmU1ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qEKwHEJXVQPsSE1LW0iZTKz8o0__7T8oSxJwT4TO-YQ"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+def get_poster_url(poster_api_path, size="w342"):
+    base_url = "https://image.tmdb.org/t/p/"
+    return f"{base_url}{size}/{poster_api_path}"
+
+def get_movies(how_many, list_type="popular"):
+    data = get_movies_list(list_type)
+    return data["results"][:how_many]
+
+API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYmIyOTgxNjYyNTNhNjI3OGRkN2IxN2UxNDk1N2MwZiIsIm5iZiI6MTc1NjgwODIyMi45MzIsInN1YiI6IjY4YjZjNDFlYzBmYjRiNjA5YmI0ZmU1ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qEKwHEJXVQPsSE1LW0iZTKz8o0__7T8oSxJwT4TO-YQ"
+
+def get_single_movie(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+def get_single_movie_cast(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()["cast"]
+
+def get_movie_images(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+def get_movies_list(list_type):
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    response.raise_for_status()
+    return response.json()
